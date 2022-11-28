b099fab86d48048a5d08f67fb4b8a318e6b9479d
Dilshod004 <muhammadiyevdilshod842@gmail.com>
2022-11-29 00:55:42 +0500

    '9-dars'

---
+++ dars-9.py
@@ -0,0 +1,36 @@
+
+
+# while takrorlanish sikli (operatori)
+
+
+#while bu toki degan ma'moni anglatib, True qiymat olsa - abadiy ishlayveradi
+# False yoki biror chegara berilsa, shu chegaraga borib to'xtaydi
+
+#####################################################################################
+
+
+# # i o'zgaruvchi berib, uni 1ga tenglashtiramiz
+# i = 1
+# # ya'ni i o'zgaruvchi 6 soniga borgunicha
+# while i <= 6:
+# # shu i sonni qaytarsin (print)
+#   print(i)
+#   # har birini birga qo'shib ketaversin
+#   i += 2
+
+
+
+
+#####################################################################################
+
+
+
+# break kalit so'zi - berilgan o'zgaruvchi yoki buyruq operatoriga shu buyruq berilsa, hammasini to'xtatadi
+
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 1:
+    break
+  i += 1
